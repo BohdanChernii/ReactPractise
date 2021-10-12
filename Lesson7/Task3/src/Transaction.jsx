763dff2c8c44086017f661e8ea05050e5de5e6b4
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-12 17:41:33 +0300

    nn

--- Lesson7/Task3/src/Transaction.jsx
+++ Lesson7/Task3/src/Transaction.jsx
@@ -3,14 +3,14 @@ import moment from 'moment';
 
 const Transaction = ({ rate, time, amount, from, to }) => {
   return (
-    <li class="transaction">
-      <span class="transaction__date">10 Jan</span>
-      <span class="transaction__time">{moment(time).format('HH:mm')}</span>
-      <span class="transaction__assets">
+    <li className="transaction">
+      <span className="transaction__date">10 Jan</span>
+      <span className="transaction__time">{moment(time).format('HH:mm')}</span>
+      <span className="transaction__assets">
         {from} → {to}
       </span>
-      <span class="transaction__rate">{Intl.NumberFormat('en-GB').format(rate)}</span>
-      <span class="transaction__amount">{Intl.NumberFormat('en-GB').format(amount)}</span>
+      <span className="transaction__rate">{new Intl.NumberFormat('en-GB').format(rate)}</span>
+      <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>
     </li>
   );
 };
