04330beaefbe5f4227f4648a49c0ff5645ca6deb
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-12 14:55:06 +0300

    aa

--- Lesson7/Task2/src/UsersList.jsx
+++ Lesson7/Task2/src/UsersList.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import User from './User.jsx';
 
-function UsersList({ users }) {
-  return (
-    <div>
-      <button className="btn">-</button>
-      <ul className="users">
-        {users.map(user => (
-          <User key={users.name} name={user.name} age={user.age} />
-        ))}
-      </ul>
-    </div>
-  );
+//algo
+
+//1) Повинен мінятись текст кнопки
+//2) згідно тексту який в кнопці виконувати операції
+
+class UsersList extends Component {
+  state = {
+    sorting: null,
+  };
+
+  toggleSort = () => {
+    this.setState({
+      sorting: this.state.sorting === 'asc' ? 'desc' : 'asc',
+    });
+  };
+
+  render() {
+    let usersList;
+    if (this.state.sorting) {
+      usersList = this.props.users.sort((a, b) =>
+        this.state.sorting === 'asc' ? a.age - b.age : b.age - a.age,
+      );
+    } else {
+      usersList = this.props.users;
+    }
+    return (
+      <div>
+        <button className="btn" onClick={this.toggleSort}>
+          {this.state.sorting || '-'}
+        </button>
+        <ul className="users">
+          {this.props.users.map(user => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 export default UsersList;
