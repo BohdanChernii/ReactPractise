6040b1f4eecfa3add58de0e35f30a15370e68edf
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-13 21:40:23 +0300

    event

---
+++ Lesson8/Task3/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Dimensions from './Dimensions';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <Dimensions />
+    );
+  }
+}
+export default App;
---
+++ Lesson8/Task3/src/Dimensions.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Dimensions extends Component {
+  constructor(props) {
+    super(props);
+    const { innerHeight, innerWidth } = window;
+    this.state = {
+      width: innerWidth,
+      height: innerHeight,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+
+    this.setState({
+      width: innerWidth,
+      height: innerHeight,
+    });
+    document.title=`${innerWidth} x ${innerHeight}`;
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  render() {
+    return <div className="dimensions">{`${this.state.width}px -  ${this.state.height}px`}</div>;
+  }
+}
+export default Dimensions;
