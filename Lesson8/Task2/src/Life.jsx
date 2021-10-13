1519607836d2040eed1f82137807af8e54021587
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-13 12:52:45 +0300

    true

--- Lesson8/Task2/src/Life.jsx
+++ Lesson8/Task2/src/Life.jsx
@@ -18,7 +18,6 @@ class Life extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-    return true;
   }
 
   componentWillUnmount() {
