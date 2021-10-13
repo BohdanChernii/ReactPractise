06a72ae276ae0c88683d0df6f9c6288092360965
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-13 12:59:11 +0300

    should

--- Lesson8/Task1/src/Life.jsx
+++ Lesson8/Task1/src/Life.jsx
@@ -14,11 +14,12 @@ class Life extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return true;
   }
 
   componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-    return true;
+   
   }
 
   componentWillUnmount() {
