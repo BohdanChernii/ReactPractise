dbb3818ddb57eafe32646142919f05298faff650
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-14 19:45:50 +0300

    clean

--- Lesson9/Task2/src/App.jsx
+++ Lesson9/Task2/src/App.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import UserForm from './UserForm';
 
 class App extends React.Component {
-createUser = user =>{
-    console.log(user)
-}
+
   render() {
-    return <UserForm onSubmit={this.createUser}/>;
+    return <UserForm />;
   }
 }
 export default App;
