2fb263c5aa8ce1a6351bf7249a9030658df01a52
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-14 17:31:45 +0300

    clean

--- Lesson9/Task1/src/index.jsx
+++ Lesson9/Task1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./index.scss"
+import './index.scss';
 import Search from './Search.jsx';
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(<Search />, root)
+ReactDOM.render(<Search />, root);
