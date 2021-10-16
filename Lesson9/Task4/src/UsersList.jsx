2bb61f4a1e12967890764e4173ccc8136fc4d1c5
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-16 18:51:56 +0300

    r

--- Lesson9/Task4/src/UsersList.jsx
+++ Lesson9/Task4/src/UsersList.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
 import User from './User.jsx';
-import Filter from './Filter.jsx'
+import Filter from './Filter.jsx';
 
 class UserForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sorting:'',
-    }
-   
+      sorting: '',
+    };
   }
-  handleChange = (e) =>{
+  handleChange = e => {
     this.setState({
       sorting: e.target.value,
-    })
-  }
-  
+    });
+  };
+
   render() {
-    let usersList = this.props.users
-    const result = (usersList.filter(user => user.name.toLowerCase().includes(this.state.sorting.toLocaleLowerCase()))
-      .map((user) => ( 
-      <User key={user.id} {...user} />
-      )))
+    let usersList = this.props.users;
+    const result = usersList
+      .filter(user => user.name.toLowerCase().includes(this.state.sorting.toLocaleLowerCase()))
+      .map(user => <User key={user.id} {...user} />);
     return (
       <div>
-        {<Filter filterText = {this.state.sorting} onChange={this.handleChange} count={result.length}/> }
-        <ul className="users">
-         {result}
-        </ul>
+        {
+          <Filter
+            filterText={this.state.sorting}
+            onChange={this.handleChange}
+            count={result.length}
+          />
+        }
+        <ul className="users">{result}</ul>
       </div>
     );
   }
