0069fe10f97f6042834f64e675c5da5caade14ed
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-16 23:06:54 +0300

    qw

--- Lesson10/Task2/src/CartTitle.jsx
+++ Lesson10/Task2/src/CartTitle.jsx
@@ -3,7 +3,7 @@ import React  from 'react';
 function CartTitle ({ userName, count}) {
         return (
             <div className="cart-title">
-             <h1 className="title">{`${userName}, you added ${count} items`}</h1>
+             {`${userName}, you added ${count} items`}
             </div>
         )
 }
