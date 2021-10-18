fe2f6445111835919d481237b98ef4d109f8e269
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-18 19:38:47 +0300

    hidde button (dialog window)

---
+++ Lesson11/Task2/src/Dialog.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Dialog({ isOpen, title, children, onClose }) {
+  if(!isOpen) {
+      return null;  
+  }
+    return (
+    <div className="dialog">
+      <div className="dialog__heading">
+        <h4 className="dialog__title">{title}</h4>
+        <button className="dialog__close-btn" onClick={ onClose}>+</button>
+      </div>
+      <div className="dialog__content">
+        <p>{children}</p>
+      </div>
+  
+    </div>
+  );
+}
+
+Dialog.propTypes = {
+    isOpen: PropTypes.bool,
+    children: PropTypes.element,
+    title:PropTypes.string,
+    onClose: PropTypes.func.isRequired
+}
+
+Dialog.defaultProps = {
+    isOpen:false,
+    title: '',
+}
+
+export default Dialog;
