6e55e6eebe8860a58f74663e8193d805a218c564
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-18 18:31:11 +0300

    asds

--- Lesson11/Task1/src/Numbers.jsx
+++ Lesson11/Task1/src/Numbers.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Numbers extends React.Component {
-    shouldComponentUpdate(nextProps){
-        if(this.props.number === nextProps.number && this.props.title ===  nextProps.title){
-            return false
-        }
-        return true;
+  shouldComponentUpdate(nextProps) {
+    if (this.props.number === nextProps.number && this.props.title === nextProps.title) {
+      return false;
     }
-    render() {
-        const {title, number} = this.props
-        return (
-            <div className="number">
-            <span className="number__title">{title}</span>
-            <span className="number__value">{number}</span>
-          </div>  
-        )
-    }
+    return true;
+  }
+  render() {
+    const { title, number } = this.props;
+    return (
+      <div className="number">
+        <span className="number__title">{title}</span>
+        <span className="number__value">{number}</span>
+      </div>
+    );
+  }
 }
+
+Numbers.PropTypes = {
+  title: PropTypes.string,
+  number: PropTypes.number.isRequired,
+};
+
+Numbers.defaultProps = {
+    title:'Some number',
+    number: PropTypes.number.isRequired,
+  };
+
 export default Numbers;
