223372724aca8307a031494a7d20288a9d1a68aa
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-20 13:27:33 +0300

    changeg classes

--- TodoList/src/Task.jsx
+++ TodoList/src/Task.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import classNames from 'classNames';
+// import classNames from 'classNames';
 import PropTypes from 'prop-types';
 
 function Task({ done, text, onChange, onDelete, id }) {
-  const listItemClasses = classNames('list-item', { 'list-item_done': done });
+  // const listItemClasses = classNames('list-item', { 'list-item_done': done });
   return (
-    <li className={listItemClasses}>
-      <input type="checkbox" 
-      className="list-item__checkbox" 
-      defaultChecked={done} 
-      onChange={()=>onChange(id)} 
+    <li className={done?'list-item list-item_done':'list-item'}>
+      <input
+        type="checkbox"
+        className="list-item__checkbox"
+        defaultChecked={done}
+        onChange={() => onChange(id)}
       />
       <span className="list-item__text">{text}</span>
-      <button className="list-item__delete-btn" onClick={()=>onDelete(id)}></button>
+      <button className="list-item__delete-btn" onClick={() => onDelete(id)}></button>
     </li>
   );
 }
-Task.PropTypes={
+Task.PropTypes = {
   done: PropTypes.bool,
   text: PropTypes.string,
   onDelete: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
-  id:PropTypes.number.isRequired
-}
+  id: PropTypes.number.isRequired,
+};
 Task.defaultProps = {
-  text:'',
-  done:false,
-}
-
-
+  text: '',
+  done: false,
+};
 
 export default Task;
