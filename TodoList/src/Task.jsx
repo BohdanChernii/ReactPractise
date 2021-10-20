709a49370fe18cef4e2ce49b4a475277f9e09ebd
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-20 13:11:44 +0300

    todo

--- TodoList/src/Task.jsx
+++ TodoList/src/Task.jsx
@@ -6,7 +6,11 @@ function Task({ done, text, onChange, onDelete, id }) {
   const listItemClasses = classNames('list-item', { 'list-item_done': done });
   return (
     <li className={listItemClasses}>
-      <input type="checkbox" className="list-item__checkbox" defaultChecked={done} onChange={()=>onChange(id)} />
+      <input type="checkbox" 
+      className="list-item__checkbox" 
+      defaultChecked={done} 
+      onChange={()=>onChange(id)} 
+      />
       <span className="list-item__text">{text}</span>
       <button className="list-item__delete-btn" onClick={()=>onDelete(id)}></button>
     </li>
