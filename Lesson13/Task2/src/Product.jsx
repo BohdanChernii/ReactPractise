43c798e7437d169816e6727e52129f46b75ef563
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-22 16:22:44 +0300

    Router2

--- Lesson13/Task2/src/Product.jsx
+++ Lesson13/Task2/src/Product.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Product({match}) {
   return (
-    <div className="product">{`Product is a ${match.params.productId}`}</div>
+    <div className="product">{match.params.productId}</div>
   );
 }
 export default Product;
